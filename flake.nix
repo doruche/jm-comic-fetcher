a7a8e344ab79c9e26829e930542e3cc2d6ff60d4
doruche <doruche18@outlook.com>
2026-09-07 22:07:36 +0800

    chore: initialize MIT plugin with uv and Nix development environment

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "JM Comic Fetcher development shell (uv only)";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+    in
+    {
+      devShells = nixpkgs.lib.genAttrs systems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [ pkgs.uv ];
+            UV_PYTHON_PREFERENCE = "only-managed";
+          };
+        }
+      );
+    };
+}
